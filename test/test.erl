347412c6025f31467a936a91f89fedc6818e37a6
Milán Bór <milanbor40@gmail.com>
2025-07-19 16:59:48 +0200

    add xml parser

--- test/test.erl
+++ test/test.erl
@@ -4,4 +4,42 @@
 
 parse_test() ->
     Path = "examples/books.xml",
-    {ok, _Xml} = ktn_xml:parse(Path).
+    {library,
+     [{id, "112233"}],
+     [{book, _, _}, {book, _, _}, {book, _, _}]} = signerl_xml:parse_file(Path).
+
+add_new_test() ->
+    Path = "examples/books.xml",
+    ExpectedPath = "examples/books_with_new.xml",
+
+    Root = signerl_xml:parse_file(Path),
+    Expected = signerl_xml:parse_file(ExpectedPath),
+
+    New =
+        signerl_xml:add_new_element(new_test_element(), Root),
+
+    ?assertEqual(
+       Expected,
+       New
+      ).
+
+export_test() ->
+    Path = "examples/books.xml",
+    PathTo = "examples/books_export_test.xml",
+    Root = signerl_xml:parse_file(Path),
+    Prolog = ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>"],
+    Binary = signerl_xml:export(Prolog, Root),
+
+    ok = signerl_xml:to_file(PathTo, Binary),
+    ?assertEqual(
+      Root,
+      signerl_xml:parse_file(PathTo)
+     ).
+
+%% Utils
+
+new_test_element() ->
+    NewTag = book,
+    NewAttrs = [{id, "3"}],
+    NewContent = [{title, [], ["My new book"]}],
+    {NewTag, NewAttrs, NewContent}.
